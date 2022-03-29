b31698b608ff71a1a29ab0685df559115e699a23
Hevia <anthony@hevia.dev>
2022-03-29 07:41:29 -0700

    run autoformatter + other work

--- src/topic_clustering.py
+++ src/topic_clustering.py
@@ -2,6 +2,7 @@ from bertopic import BERTopic
 from scikg_types.DocumentTypes import TextFile
 from utils.FileHelpers import loadPickle, savePickle
 
+
 def createTopicIndex(textFiles: list[TextFile]) -> dict[str, list[str]]:
     topic_model = BERTopic()
 
@@ -9,15 +10,15 @@ def createTopicIndex(textFiles: list[TextFile]) -> dict[str, list[str]]:
     # TODO: See if removing stopwords improves cluster performance, or maybe just need more articles?
     # https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
     for textFile in textFiles:
-        docs.append(textFile['file_content'])
-    
+        docs.append(textFile["file_content"])
+
     topics, probs = topic_model.fit_transform(docs)
 
     topicIndex: dict[str, list[str]] = {}
-    
+
     for topic, textFile in zip(topics, textFiles):
         if topic in topicIndex:
-                topicIndex[topic].append(textFile["file_id"])
+            topicIndex[topic].append(textFile["file_id"])
         else:
             topicIndex[topic] = [textFile["file_id"]]
 
@@ -26,20 +27,29 @@ def createTopicIndex(textFiles: list[TextFile]) -> dict[str, list[str]]:
 
     return topicIndex
 
+
 # TODO: Make this not a cold start
-def inferTopic(text: str): 
+def inferTopic(text: str):
     topic_model = BERTopic.load("./models/topic-model")
     topicIndex = loadPickle("./models/indexes/topicIndex.pickle")
     topics, _ = topic_model.transform([text])
 
-    print(f"Topics found for associated text: {topics}")
+    # print(f"Topics found for associated text: {topics}")
 
+    results_dict: dict[str, list[str]] = {}
     for topic in topics:
-        if topic in topicIndex:
-            print(f"Topic: {topic}, related files: {topicIndex[topic]}")
+        if topic in topicIndex and topic not in results_dict:
+            print(f"Results found for topic: {topic}")
+            results_dict[topic] = topicIndex[topic]
+        elif topic in topicIndex and topic in results_dict:
+            print(f"Found duplicate topic, adding additional results for: {topic}")
+            results_dict[topic].extend(topicIndex[topic])
+
+    return results_dict
+
 
 def loadAndPrint():
     topic_model = BERTopic.load("./models/topic-model")
 
-    freq = topic_model.get_topic_info(); 
+    freq = topic_model.get_topic_info()
     print(freq.head(5))
--- src/utils/FileHelpers.py
+++ src/utils/FileHelpers.py
@@ -8,20 +8,23 @@ def loadTextFilesBulk(dir_path: str) -> list[TextFile]:
     try:
         for _, _, file_names in os.walk(dir_path):
             for file_name in file_names:
-                textFiles.append({
-                    "file_id": file_name,
-                    "file_content": loadFile(f"{dir_path}/{file_name}")
-                })
+                textFiles.append(
+                    {
+                        "file_id": file_name,
+                        "file_content": loadFile(f"{dir_path}/{file_name}"),
+                    }
+                )
     except Exception as e:
         # TODO logging
         print(e)
 
     return textFiles
 
+
 def loadFile(file_path: str) -> str:
     """
     Loads the file at a specificed path, returns an error otherwise
-    
+
     :param file_path (str): The path to the file you want to load
     :return _file a str of the contents of the file
     """
@@ -33,10 +36,11 @@ def loadFile(file_path: str) -> str:
     except Exception as e:
         raise Exception(f"Error: {e} file at: {file_path}")
 
+
 def loadFileAsLines(file_path: str) -> list[str]:
     """
     Loads a \n separated text file at a specificed path, returns an error otherwise
-    
+
     :param file_path (str): The path to the file you want to load
     :return _file a list[str] separated by \n
     """
@@ -48,6 +52,7 @@ def loadFileAsLines(file_path: str) -> list[str]:
     except Exception as e:
         raise Exception(f"Error: {e} reading file at: {file_path}")
 
+
 def saveTextFile(data, save_path) -> None:
     try:
         with open(save_path, "w+", encoding="utf-8") as fp:
@@ -55,9 +60,10 @@ def saveTextFile(data, save_path) -> None:
     except Exception:
         raise Exception(f"Error with trying to save file at: {save_path}")
 
+
 def savePickle(data, save_path) -> None:
     try:
-        with open(save_path, 'wb') as f:
+        with open(save_path, "wb") as f:
             pickle.dump(data, f)
     except Exception as e:
         raise Exception(f"Error: {e} with trying to save pickle at: {save_path}")
